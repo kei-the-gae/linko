ceef8e50dda7d22a862503cc8c900b7f8b1cef54
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2026-06-25 13:00:47 -0700

    obfuscate client ips in logs

--- server_test.go
+++ server_test.go
@@ -22,20 +22,24 @@ func Test_requestLogger(t *testing.T) {
 	}))
 
 	requestLoggerMiddleware := requestLogger(logger)
+
 	dummyHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+
 	loggedHandler := requestLoggerMiddleware(dummyHandler)
 
-	req := httptest.NewRequest("GET", "http://lin.ko/api/stats", nil)
+	req := httptest.NewRequest("GET", "http://localhost:8080/test?foo=bar", nil)
+
 	rr := httptest.NewRecorder()
+
 	loggedHandler.ServeHTTP(rr, req)
 
-	const expectedLogString = `time=2023-10-01T12:34:57.000Z level=INFO msg="Served request" method=GET path=/api/stats client_ip=192.0.2.1:1234` + "\n"
-	const expectedStatusCode = http.StatusOK
-
-	if rr.Code != expectedStatusCode {
-		t.Errorf("Expected status code %d, got %d", expectedStatusCode, rr.Code)
+	if rr.Code != http.StatusOK {
+		t.Errorf("Expected status code 200, got %d", rr.Code)
 	}
-	if logBuffer.String() != expectedLogString {
-		t.Errorf("Expected log output:\n%s\nGot:\n%s", expectedLogString, logBuffer.String())
+
+	expectedLog := `time=2023-10-01T12:34:57.000Z level=INFO msg="Served request" method=GET path=/test client_ip=192.0.2.x
+`
+	if logBuffer.String() != expectedLog {
+		t.Errorf("Expected log output:\n%s\nGot:\n%s", expectedLog, logBuffer.String())
 	}
 }
